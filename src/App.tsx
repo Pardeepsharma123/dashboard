e71a6b95c71cc3581f98893d3a861f95e9517fae
Pardeepsharma123 <pardeepsharma529@gmail.com>
2022-02-03 14:04:19 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import MyDropzone from './component/MyDropzone';
+import React,{useState} from 'react'
+import MyDropzone from './component/dragdrop/MyDropzone';
+import CommentModal from './component/message/CommentModal'
+import DashboardCard from './component/dashboard/DashboardCard'
+import DashboardList from './component/dashboard/DashboardList'
+
+const assets_list=["Total Uploaded Assets","Data Changes","Data Quries","Provisinal Results"]
+
 function App() {
+  const [showCommentBox,setShowCommentBox]=useState(false)
   return (
     <div className="App">
-     <MyDropzone/>
+     {/* <MyDropzone/> */}
+     <CommentModal/>
+     <DashboardList list={assets_list}/>
+     {/* <DashboardCard title='Total Uploaded Assets'/> */}
     </div>
   );
 }
